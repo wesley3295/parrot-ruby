607cf6b9faa51cf1da587872614fc6e8836ae607
Wesley <wspencer3295@yahoo.com>
2020-11-09 20:21:21 -0700

    commit tree 0c46b868b673d66b7edd7b9fe3ea0552350eb488

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,12 @@
 # Create method `parrot` that outputs a given phrase and
-# returns the phrase
+# returns the phr
+def parrot(phrase= nil)
+    if phrase
+    puts phrase
+    return phrase
+    else
+       puts "Squawk!"
+    return "Squawk!" 
+    end
+end
+
